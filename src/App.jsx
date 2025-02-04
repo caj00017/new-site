7f422c2964707f2699b3620774d7bc256cfc92c4
caj00017 <caj00017@mix.wvu.edu>
2025-02-04 11:02:26 -0500

    title page v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import TypeIt from 'typeit'
+import { useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    new TypeIt("#title", {
+      speed: 25,
+      waitUntilVisible: true,
+    }).go();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="item">
+          <h1 id="title">Hi, my name is Chris!<br />This is my website!</h1>
+        </div>
+        <div className="item">
+          <ul>
+            <li>This website is a work in progress.</li>
+            <li>Come back later to see what's up!</li>
+          </ul>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
